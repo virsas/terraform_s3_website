d37a0b42be4940683cb0739a7b92fd8c29ab62f0
Stefan Moravcik <stefan@virsas.com>
2022-04-25 19:37:04 +0200

    added region

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_s3_bucket_website_configuration" "bucket" {
   bucket = var.bucket
 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
+variable "region" { default = "eu-west-1" }
 variable "bucket" { default = "" }
 variable "index_page" { default = "index.html" }
 variable "error_page" { default = "error.html" }
